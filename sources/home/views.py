9ef55cb9778a222758e692b45e166b5c825f4e0e
Nolan Gregory <65730528+nulzo@users.noreply.github.com>
2023-08-08 01:38:33 -0500

    some work

--- sources/home/views.py
+++ sources/home/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, template_name="home/home.html")
+    text = {
+        "Track Your Spending": "Track that thang yup!",
+        "Stay on Budget": "I am some samlpe text",
+    }
+    context = {"text": text}
+    return render(request, template_name="home/home.html", context=context)
